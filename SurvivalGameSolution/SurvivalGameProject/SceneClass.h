1def4ffd61d8146d9743f7a792894457b61d9746
ChartaP <paper2402@gmail.com>
2016-08-03 11:13:18 +0900

    add SceneClass and Coding that

---
+++ SurvivalGameSolution/SurvivalGameProject/SceneClass.h
@@ -0,0 +1,17 @@
+#pragma once
+class SceneClass
+{
+public:
+	SceneClass *PrevScene;
+	TCHAR name[8];
+
+	virtual VOID FadeIn() = 0;
+	virtual VOID FadeOut() = 0;
+
+	virtual VOID Enter() = 0;
+	virtual VOID Update() = 0;
+	virtual VOID Exit() = 0;
+
+private:
+
+};
